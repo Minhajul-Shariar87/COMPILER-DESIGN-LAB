fc96c9fcd75b3a88d6bc0dc5d10b9ce52968f513
Minhajul-Shariar87 <shariarzaman11@gmail.com>
2026-03-05 10:17:17 +0600

    Lab 4

---
+++ LAB 4/task1.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+#include<fstream>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+
+bool isVariable(string s){
+for(int i=0;i<s.size();i++){
+if(s[i]==' '){
+  if(isalpha(s[i+1])||s[i+1]=='_'){
+    return true;
+}else{
+return false;
+}
+}
+
+}
+
+}
+
+bool isFunction(string s){
+for(int i=0;i<s.size();i++){
+    if(s){}
+}
+}
+int main(){
+//ofstream Input("input.txt"); // Go to file then input
+//ofstream Valid_Code("valid_code.txt"); //Write Only
+ofstream Report("report.txt"); //Read Only
+string input;
+string s="";
+ifstream readfile("input.txt");
+while(getline(readfile,input)){
+     s=input;
+     if(s[s.size()-1]==')'&&s[s.size()-2]=='('){
+        isFunction(s);
+     }else{
+     isVariable(s)
+     }
+}
+
+Report.close();
+
+
+
+
+
+
+return 0;
+}
+
